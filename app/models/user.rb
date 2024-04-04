86001e3e7cb3c5a49a3147031a7a063a4de11de1
hikijikaito <kaito.happy.19971121@gmail.com>
2024-04-04 15:34:44 +0900

    新規登録、ログイン、ログアウト

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+    validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
+    validates :password_confirmation
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
+end
