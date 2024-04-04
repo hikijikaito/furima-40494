ce6a8dc42b2c73c879ef84c53692796bc1adbbbc
hikijikaito <kaito.happy.19971121@gmail.com>
2024-04-04 17:05:21 +0900

    テストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
+    validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
